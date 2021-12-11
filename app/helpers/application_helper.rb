0dd5842a03f7b77452eca997af3949d966986cde
AleksandrOparin <sasha.oparin.1984@mail.ru>
2021-12-12 01:13:03 +0300

    initial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def nav_tab(title, icon, url, options = {}, &block) 
+  def nav_tab(title, icon, url, options = {}) 
     current_page = options.delete :current_page
     css_class = current_page == title ? 'active disabled' : ''
     
